c1430fb65b8dcc6cc26b825009a7fd61fa0df0c3
Zhang Ze <zhangze@rvcore.com>
2025-02-08 16:20:04 +0800

    add layer.conf and setup.sh
    Signed-off-by: Zhang Ze <zhangze@rvcore.com>

---
+++ setup.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Bootstrapper for buildbot slave
+
+DIR="build"
+MACHINE="qemuriscv64"
+DISTRO="poky-altcfg"
+CONFFILE="conf/auto.conf"
+# core-image-sato, corea-image-sato-sdk
+BITBAKEIMAGE="core-image-full-cmdline"
+
+# make sure sstate is there
+#echo "Creating sstate directory"
+#mkdir -p ~/sstate/$MACHINE
+
+# fix permissions set by buildbot
+#echo "Fixing permissions for buildbot"
+#umask -S u=rwx,g=rx,o=rx
+#chmod -R 755 .
+
+# Reconfigure dash on debian-like systems
+which aptitude > /dev/null 2>&1
+ret=$?
+if [ "$(readlink /bin/sh)" = "dash" -a "$ret" = "0" ]; then
+  sudo aptitude install expect -y
+  expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
+elif [ "${0##*/}" = "dash" ]; then
+  echo "dash as default shell is not supported"
+  return
+fi
+# bootstrap OE
+echo "Init OE"
+export BASH_SOURCE="poky/oe-init-build-env"
+. ./poky/oe-init-build-env $DIR
+
+# Symlink the cache
+#echo "Setup symlink for sstate"
+#ln -s ~/sstate/${MACHINE} sstate-cache
+
+# add the missing layers
+echo "Adding layers"
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-openembedded/meta-python
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+bitbake-layers add-layer ../meta-riscv
+bitbake-layers add-layer ../meta-rvcore
+
+# fix the configuration
+echo "Creating auto.conf"
+
+if [ -e $CONFFILE ]; then
+    rm -rf $CONFFILE
+fi
+cat <<EOF > $CONFFILE
+MACHINE ?= "${MACHINE}"
+DISTRO = "${DISTRO}"
+#IMAGE_FEATURES += "tools-debug"
+#IMAGE_FEATURES += "tools-tweaks"
+#IMAGE_FEATURES += "dbg-pkgs"
+# rootfs for debugging
+#IMAGE_GEN_DEBUGFS = "1"
+#IMAGE_FSTYPES_DEBUGFS = "tar.gz"
+PACKAGECONFIG:append:pn-qemu-native = " sdl"
+PACKAGECONFIG:append:pn-nativesdk-qemu = " sdl"
+USER_CLASSES:append = " buildstats buildhistory buildstats-summary"
+EOF
+
+echo "To build an image run"
+echo "---------------------------------------------------"
+echo "bitbake core-image-full-cmdline"
+echo "---------------------------------------------------"
+echo ""
+echo "Buildable machine info"
+echo "---------------------------------------------------"
+echo "* qemuriscv64: The 64-bit RISC-V machine"
+echo "* qemuriscv32: The 32-bit RISC-V machine"
+echo "* freedom-u540: The SiFive HiFive Unleashed board"
+echo "---------------------------------------------------"
+
+# start build
+#echo "Starting build"
+#bitbake $BITBAKEIMAGE
+
